c601daf1213b309cfa0a7a6ce854419f8c6cad51
kenembanisi <kene.mbanisi@gmail.com>
2025-01-22 12:48:46 -0500

    temp dev.4

--- gamepad_control.py
+++ gamepad_control.py
@@ -56,9 +56,17 @@ class GamepadControl():
                     self.ARM_FLAG = bool(event.state)
         
         if self.MOBILE_BASE_FLAG:
-            gamepad_cmds.base_vx = self.abs_x
-            gamepad_cmds.base_vy = self.abs_y
-            gamepad_cmds.base_w = self.abs_z
+
+            # we set the range for vx, vy to -1 m/s to 1 m/s
+            # w = -0.5 rad/s to 0.5 rad/s
+
+            gamepad_cmds.base_vx = self.map_value(self.abs_x, -1, 1)
+            gamepad_cmds.base_vy = self.map_value(self.abs_y, -1, 1)
+            gamepad_cmds.base_w = self.map_value(self.abs_z, -0.5, 0.5)
+
+            print(f'vx = [{self.abs_x} -> {gamepad_cmds.base_vx}], \
+                   vy = [{self.abs_y} -> {gamepad_cmds.base_vy}] \
+                   w = [{self.abs_z} -> {gamepad_cmds.base_w}]')
 
         if self.ARM_FLAG:
             gamepad_cmds.arm_vx = self.abs_x
@@ -66,3 +74,9 @@ class GamepadControl():
             gamepad_cmds.arm_vz = self.abs_z
         
         return gamepad_cmds
+
+
+    def map_value(self, x: float, hw_min=0, hw_max=1):
+        # hw_min, hw_max = 0, 1000 # defined by the driver
+        joint_min, joint_max = 0, 255
+        return int((x - joint_min) * (hw_max - hw_min) / (joint_max - joint_min) + hw_min)
